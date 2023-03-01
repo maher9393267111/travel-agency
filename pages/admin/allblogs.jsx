206d23ee78ed2aadabd4a150a268112b7d9b8d40
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-03-01 11:26:32 +0300

    appp

---
+++ pages/admin/allblogs.jsx
@@ -0,0 +1,51 @@
+import dynamic from "next/dynamic";
+import Seo from "../../components/common/seo";
+import MyFavourites from "../../components/dashboard/my-favourites";
+import { db } from "../../firebase";
+
+const AdminBlogs = ({  data}) => {
+
+    console.log('data', data);
+
+
+
+  return (
+    <>
+      <Seo pageTitle="My Favourites" />
+      <MyFavourites />
+    </>
+  );
+};
+
+export default AdminBlogs;
+
+
+export async function getStaticProps() {
+    const data = [];
+  
+    try {
+      const querySnapshot = await getDocs(collection(db, 'blog'));
+  
+      querySnapshot.forEach((doc) => {
+        data.push({
+          id: doc.id,
+          ...doc.data(),
+        });
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  
+    return {
+      props: {
+        data,
+      },
+      revalidate: 60,
+    };
+  }
+
+
+
+
+
+// export default dynamic(() => Promise.resolve(index), { ssr: false });
