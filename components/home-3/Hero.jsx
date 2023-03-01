b47f97c3bd78e4f3f6cc16288e06ffd8f712330e
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-03-01 17:15:17 +0300

    add slogan in banner

--- components/home-3/Hero.jsx
+++ components/home-3/Hero.jsx
@@ -6,11 +6,11 @@ const Hero = () => {
     <section className="home-three bg-img3 mt85 md-mt0">
       <div className="container">
         <div className="row">
-          <div className="col-lg-8">
-            <div className="home3_home_content">
-              <h1>Your Property, Our Priority.</h1>
-              <h4>
-                From as low as $10 per day with limited time offer discounts
+          <div className="col-lg-8  ">
+            <div className="home3_home_content ml-12">
+              <h1>Patagonia</h1>
+              <h4 className="  text-3xl">
+                Enjoy  Journey ,Not The destination
               </h4>
             </div>
           </div>
