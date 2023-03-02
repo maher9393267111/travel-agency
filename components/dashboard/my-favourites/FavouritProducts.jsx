d8554efb3db8a8b2af18911f0dc5c0ef1e7c1ac7
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-03-02 20:31:02 +0300

    add blog details in admin dashboard

--- components/dashboard/my-favourites/FavouritProducts.jsx
+++ components/dashboard/my-favourites/FavouritProducts.jsx
@@ -15,77 +15,12 @@ import { StateContext } from "../../../context/index";
 import properties from "../../../data/properties";
 
 const FavouritProducts = ({blogs}) => {
-  let content = properties?.slice(0, 4)?.map((item) => (
-    <div className="feat_property list favorite_page" key={item.id}>
-      <div className="thumb">
-        <img className="img-whp cover" src={item.img} alt="fp1.jpg" />
-        <div className="thmb_cntnt">
-          <ul className="tag mb0">
-            <li className="list-inline-item">
-              <a href="#">For Rent</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-      {/* End .thumb */}
-
-      <div className="details">
-        <div className="tc_content">
-          <h4>
-            {" "}
-            <Link href={`/listing-details-v1/${item.id}`}>
-              <a>{item.title}</a>
-            </Link>
-          </h4>
-          <p>
-            <span className="flaticon-placeholder"></span> {item.location}
-          </p>
-          <a className="fp_price text-thm" href="#">
-            ${item.price}
-            <small>/mo</small>
-          </a>
-        </div>
-      </div>
-      {/* End details */}
-
-      <ul className="view_edit_delete_list mb0 mt35">
-        <li
-          className="list-inline-item"
-          data-toggle="tooltip"
-          data-placement="top"
-          title="Delete"
-        >
-          <a href="#">
-            <span className="flaticon-garbage"></span>
-          </a>
-        </li>
-      </ul>
-      {/* view_edit_delete_list */}
-    </div>
-  ));
-
+ 
 
   const [loading,setLoading] = useState(false)
   const {  handleDelete } = useContext(StateContext);
 
 
-  // const handleDelete = async (blog) => {
-  //   try {
-
-  //     setLoading(true);
-    
-  //     await deleteDoc(doc(db, "blog", blog.id));
-  //     console.log("Document successfully deleted!");
-  //     toast.success('Blog deleted successfully')
-
-  //   } catch (error) {
-  //     console.error("Error removing document: ", error);
-  //     toast.error({message:error})
-
-  //     setLoading(false)
-  //   }
-    
-  // };
 
 
 
@@ -108,7 +43,7 @@ const FavouritProducts = ({blogs}) => {
         <div className="tc_content">
           <h4>
             {" "}
-            <Link href={`/admin/blog/${item.id}`}>
+            <Link href={`/blogs/${item.id}`}>
               <a>{item.title}</a>
             </Link>
           </h4>
