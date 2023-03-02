6f11854badd03bfb74f5b6ebc61c4208487a8bcc
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-03-02 21:00:32 +0300

    dete copyright footer fromcountry page

--- components/services/index.jsx
+++ components/services/index.jsx
@@ -70,11 +70,11 @@ const index = ({  filteredService}) => {
       </section>
 
       {/* <!-- Our Footer Bottom Area --> */}
-      <section className="footer_middle_area pt40 pb40">
+      {/* <section className="footer_middle_area pt40 pb40">
         <div className="container">
           <CopyrightFooter />
         </div>
-      </section>
+      </section> */}
     </>
   );
 };
