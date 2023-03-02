d962b6c3a7bf4c26dc7e66efc27e449c496ebda4
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-03-02 18:53:54 +0300

    update footer in login page

--- components/login/index.jsx
+++ components/login/index.jsx
@@ -44,11 +44,11 @@ const index = () => {
       </section>
 
       {/* <!-- Our Footer Bottom Area --> */}
-      <section className="footer_middle_area pt40 pb40">
+      {/* <section className="footer_middle_area pt40 pb40">
         <div className="container">
           <CopyrightFooter />
         </div>
-      </section>
+      </section> */}
     </>
     
   );
