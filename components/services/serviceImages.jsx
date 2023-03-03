4e90ce69a195d65463fffa56e6ee011693059068
maher9393267111 <89878167+maher9393267111@users.noreply.github.com>
2023-03-03 12:01:17 +0300

    up links

--- components/services/serviceImages.jsx
+++ components/services/serviceImages.jsx
@@ -9,7 +9,7 @@ const ServiceImages = ({images}) => {
     <>
       {findProperties.slice(0, 4).map((item) => (
         <div className={`col-lg-4 ${item.column}`} key={item.id}>
-          <Link href="/listing-grid-v1">
+          {/* <Link href="/"> */}
             <a className="properti_city d-block">
               <div className="thumb">
                 <img className="img-fluid w100" src={item.img} alt="pc1.jpg" />
@@ -21,7 +21,7 @@ const ServiceImages = ({images}) => {
                 </div>
               </div>
             </a>
-          </Link>
+          {/* </Link> */}
         </div>
       ))}
     </>
